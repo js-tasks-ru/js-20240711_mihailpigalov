e00ded1f1c80976233bcd17d5ee0bf8a2ec9b38c
Михаил Пигалов <pigalovmihail@yandex.ru>
2024-08-09 12:04:31 +0300

    правки

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -8,32 +8,55 @@ class Tooltip {
   }
 
   initialize() {
-    document.body.addEventListener('pointerover', (event) => this.showTooltip(event));
-    document.body.addEventListener('pointerout', (event) => this.hideTooltip(event));
+    document.body.addEventListener("pointerover", (event) =>
+      this.handleDocumentPointerover(event)
+    );
+    document.body.addEventListener("pointerout", (event) =>
+      this.handleDocumentPointerout(event)
+    );
   }
 
-  showTooltip(event) {
+  handleDocumentPointerover(event) {
     const target = event.target;
     const tooltipText = target.dataset.tooltip;
 
     if (tooltipText) {
       this.render(tooltipText);
-      document.body.addEventListener('pointermove', (event) => this.moveTooltip(event));
-      this.moveTooltip(event);
+      document.body.addEventListener("pointermove", (event) =>
+        this.handleDocumentPointerMove(event)
+      );
+      this.handleDocumentPointerMove(event);
     }
   }
 
-  hideTooltip(event) {
+  handleDocumentPointerout(event) {
     const target = event.target;
     const tooltipText = target.dataset.tooltip;
 
     if (tooltipText) {
       this.removeTooltip();
-      document.body.removeEventListener('pointermove', (event) => this.moveTooltip(event));
+      document.body.removeEventListener("pointermove", (event) =>
+        this.handleDocumentPointerMove(event)
+      );
     }
-  } 
+  }
 
-  moveTooltip(event) {
+  removeAllListeners() {
+    document.body.removeEventListener("pointermove", (event) =>
+      this.handleDocumentPointerMove(event)
+    );
+
+    document.body.removeEventListener("pointerover", (event) =>
+      this.handleDocumentPointerover(event)
+    );
+
+    document.body.removeEventListener("pointerout", (event) =>
+      this.handleDocumentPointerout(event)
+    );
+    
+  }
+
+  handleDocumentPointerMove(event) {
     const shift = 10;
     const x = event.clientX + shift;
     const y = event.clientY + shift;
@@ -45,8 +68,8 @@ class Tooltip {
   }
 
   render(text) {
-    this.element = document.createElement('div');
-    this.element.className = 'tooltip';
+    this.element = document.createElement("div");
+    this.element.className = "tooltip";
     this.element.textContent = text;
     document.body.append(this.element);
   }
@@ -59,8 +82,8 @@ class Tooltip {
 
   destroy() {
     this.removeTooltip();
+    this.removeAllListeners();
   }
-
 }
 
 export default Tooltip;
